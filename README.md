5123aa30a973c684d9ce37a183ac20780add277a
Martial Michel <mmichel@infotrend.com>
2023-04-25 15:12:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# tmuxer
+
+(Tool in development)
+
+Short shell script to take a set of commands and run them in parallel using `tmux`
+
+```
+./tmuxer tmuxfile directory_with_executables logs_directory
+```
+
+The tool:
+- creates a `tmuxfile` script (bash script)
+    - it will refuse to run if the script already exists
+- will only use executable programs from the `directory_with_executables`
+- will create the `logs_directory` and create files in there using the tmux window id, the executable name, and a `.log` extension
+    - it will refuse to run if the directory already exists
+- requires `script` and `tmux` to be installed on the host
+    - because of variations in the version of the base tools, this tool was only tested on Linux and will only accept to run on this plaform
+
+## Example of use
+
+Let's create a basic shell script in a `torun` directory name `scr1` (needs `fortune` installed)
+
+```
+#!/bin/bash
+fortune
+t=$[ ( $RANDOM % 10 )  + 1 ]
+sleep ${t}s
+```
+
+Let's copy it to `scr2`, `scr3`, and `scr4`
+
+```
+$ cp torun/scr{1,2,3,4}
+```
+
+Let's make `scr1`, `scr2` and `scr3` executable (we are keeping `src4` non executable for demonstration purpose)
+
+```
+$ chmod u+x torun/scr{1,2,3}
+```
+
+Let's run the script to get our base script:
+
+```
+$ ./tmuxer torun.tmux torun logs
+[--] Confirming executable files list from torun
+  [OK] scr1
+  [OK] scr2
+  [OK] scr3
+  [--] scr4 is not a file/executable, skipping
+[**] Writing output file: torun.tmux
+  [++] Added scr1
+  [++] Added scr2
+  [++] Added scr3
+Done
+```
+
+The generated script is fairly simple:
+
+```
+$ cat ./torun.tmux
+#!/bin/bash
+
+/usr/bin/tmux new-session -d -s torun_tmux 'echo "tmux started, this window will close in 10 seconds"; sleep 10'
+/usr/bin/tmux new-window -t torun_tmux: -n '1-scr1' '/usr/bin/script -e -O "logs/1-scr1.log" -c "torun/scr1"'
+/usr/bin/tmux new-window -t torun_tmux: -n '2-scr2' '/usr/bin/script -e -O "logs/2-scr2.log" -c "torun/scr2"'
+/usr/bin/tmux new-window -t torun_tmux: -n '3-scr3' '/usr/bin/script -e -O "logs/3-scr3.log" -c "torun/scr3"'
+/usr/bin/tmux attach-session -t torun_tmux
+
+```
+
+We can now run `./torun.tmux` and wait for all the scripts to run in parallel and complete. 
+When they are all complete, you will get back to your shell prompt after a `tmux` `[exited]` message. 
+The script logs are stored in the `logs` directory. 
+Each `log` file should have a `[COMMAND_EXIT_CODE="0"]` in the last line, which can be used to confirm that the scripts ended succesfully.
---
+++ tmuxer.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+usage () {
+  echo "$0 tmuxfile directory_with_executables logs_directory"
+  echo "Will create a shell script to start all the executables (from directory_with_executables) in a tmux session (named after the tmuxfile) and log the output to logs_directory"
+  echo "both tmuxfile and logs_directory must not exist, the script will create them, not overwrite them"
+  exit 1;
+}
+
+error_exit() { echo "$1"; exit 1; }
+check_file_exists() { if [ ! -f "$1" ]; then error_exit "File $1 does not exist or is not a file"; fi; }
+check_file_r() { check_file_exists "$1"; if [ ! -r $1 ]; then error_exit "File $1 is not readable"; fi; }
+check_file_w() { check_file_exists "$1"; if [ ! -w $1 ]; then error_exit "File $1 is not writable"; fi; }
+check_file_x() { check_file_exists "$1"; if [ ! -x $1 ]; then error_exit "File $1 is not writable"; fi; }
+
+check_dir_exists() { if [ ! -d "$1" ]; then error_exit "Directory $1 does not exist or is not a directory"; fi; }
+check_dir_r() { check_dir_exists "$1"; if [ ! -r $1 ]; then error_exit "Directory $1 is not readable"; fi; }
+check_dir_w() { check_dir_exists "$1"; if [ ! -w $1 ]; then error_exit "Directory $1 is not writable"; fi; }
+check_dir_x() { check_dir_exists "$1"; if [ ! -x $1 ]; then error_exit "Directory $1 is not writable"; fi; }
+
+make_dir() { if [ ! -d "$1" ]; then mkdir -p $1; fi; check_dir_w "$1"; }
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# find the tmux and script commands in path
+tmux=$(which tmux)
+if [ -z $tmux ]; then error_exit "tmux not found in path"; fi
+check_file_x $tmux
+script=$(which script)
+if [ -z $script ]; then error_exit "script not found in path"; fi
+
+# determine host OS
+unameOut="$(uname -s)"
+if [ "$unameOut" == "Linux" ]; then
+    hostos="Linux"
+elif [ "$unameOut" == "Darwin" ]; then
+    error_exit "MacOS not supported"
+elif [ "$unameOut" == "CYGWIN" ]; then
+    error_exit "Cygwin not supported"
+elif [ "$unameOut" == "MINGW" ]; then
+    error_exit "MinGW not supported"
+else
+    error_exit "Unknown host OS"
+fi
+
+# check variables
+if [ -z $1 ]; then usage; fi
+if [ -z $2 ]; then usage; fi
+if [ -z $3 ]; then usage; fi
+
+tmuxfile=$1
+execdir=$2
+logsdir=$3
+
+# create an empty tmuxfile
+if [ -e "$tmuxfile" ]; then error_exit "tmuxfile ($tmuxfile) already exists, not overwriting"; fi
+touch "$tmuxfile"
+check_file_w "$tmuxfile"
+chmod +x "$tmuxfile"
+check_file_x "$tmuxfile"
+
+# create an empty logsdir
+if [ -e $logsdir ]; then error_exit "logsdir ($logsdir) already exists, not overwriting"; fi
+make_dir "$logsdir"
+check_dir_w "$logsdir"
+
+echo "[--] Confirming executable files list from $execdir"
+# check that the execdir exists and is readable
+check_dir_r "$execdir"
+# Get list of executables from within execdir
+execdir_ls=$(ls "$execdir")
+# check that the executables are executable
+execdir_executable=()
+for execdir_file in $execdir_ls; do
+    if [ -f "$execdir/$execdir_file" ]; then
+        if [ -x "$execdir/$execdir_file" ]; then
+            check_file_x $execdir/$execdir_file # extra precaution
+            echo "  [OK] $execdir_file"
+            execdir_executable+=($execdir_file)
+        else
+            echo "  [--] $execdir_file is not a file/executable, skipping"
+        fi
+    fi
+done
+
+# check that we have at least one executable
+if [ ${#execdir_executable[@]} -eq 0 ]; then error_exit "No executable files found in $execdir"; fi
+
+# https://how-to.dev/how-to-create-tmux-session-with-a-script
+echo "[**] Writing output file: $tmuxfile"
+bn=$(basename $tmuxfile)
+# cleanup special characters from the basename to use as the tmux session id
+session=$(echo $bn | sed -e 's/[^a-zA-Z0-9]/_/g')
+
+window=0
+
+cat <<EOF > $tmuxfile
+#!/bin/bash
+
+$tmux new-session -d -s $session 'echo "tmux started, this window will close in 10 seconds"; sleep 10'
+EOF
+
+for f in ${execdir_executable[*]}; do
+    ((window++))
+
+    n=`basename $f`
+    name=$(echo $n | sed -e 's/[^a-zA-Z0-9]/_/g')
+    n="${window}-${name}"
+
+cat <<EOF >> $tmuxfile
+$tmux new-window -t $session:$windows -n '$n' '$script -e -O "$logsdir/$n.log" -c "$execdir/$f"'
+EOF
+
+    echo "  [++] Added $f"
+
+done
+
+cat <<EOF >> $tmuxfile
+$tmux attach-session -t $session
+EOF
+
+check_file_x $tmuxfile
+
+echo "Done"
